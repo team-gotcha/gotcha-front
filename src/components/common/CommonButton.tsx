2b0e713c3cc519f48b4247f90bc9368031616f14
silver <ekdldkaa@naver.com>
2024-01-04 22:37:13 +0900

    [feat] CommonButton 설정

---
+++ src/components/common/CommonButton.tsx
@@ -0,0 +1,96 @@
+import styled, { css } from 'styled-components';
+import { theme } from '../../style/theme';
+
+/**
+ * 사용예시
+ * <CommonButton color={'lineGray'} size={'large'} children="텍스트" />
+ */
+
+type ButtonColor = 'lineGray' | 'lineBlue' | 'fillBlue' | 'fillDark';
+type ButtonSize = 'large' | 'small';
+
+interface ButtonType {
+  color: ButtonColor;
+  size: ButtonSize;
+  children: string;
+  onClick?: () => void;
+}
+
+const CommonButton = ({ ...props }: ButtonType) => {
+  return <StyledButton {...props}>{props.children}</StyledButton>;
+};
+
+const StyledButton = styled.button<ButtonType>`
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ color }) => getButtonColor(color)};
+  ${({ size }) => getButtonShape(size)};
+`;
+
+export default CommonButton;
+
+const getButtonColor = (buttonColor: ButtonColor) => {
+  let backgroundColor;
+  let fontColor;
+  let borderColor;
+
+  switch (buttonColor) {
+    case 'lineGray':
+      backgroundColor = theme.colors.gray.gray100;
+      fontColor = theme.colors.gray.gray500;
+      borderColor = theme.colors.gray.gray500;
+      break;
+    case 'lineBlue':
+      backgroundColor = theme.colors.gray.gray100;
+      fontColor = theme.colors.purple.purple600;
+      borderColor = theme.colors.purple.purple600;
+      break;
+    case 'fillBlue':
+      backgroundColor = theme.colors.purple.purple600;
+      fontColor = theme.colors.gray.gray100;
+      break;
+    case 'fillDark':
+      backgroundColor = theme.colors.gray.gray300;
+      fontColor = theme.colors.gray.gray700;
+      break;
+
+    default:
+      return null;
+  }
+
+  return css`
+    background: ${backgroundColor};
+    color: ${fontColor};
+    border-color: ${borderColor};
+  `;
+};
+
+const getButtonShape = (buttonSize: ButtonSize) => {
+  let borderRadius;
+  let fontStyle;
+  let height;
+
+  switch (buttonSize) {
+    case 'large':
+      height = '5.2rem';
+      borderRadius = '2.2rem';
+      fontStyle = theme.fontStyles.button.buttonLarge;
+      break;
+    case 'small':
+      height = '4.4rem';
+      borderRadius = '2rem';
+      fontStyle = theme.fontStyles.subtitle.subtitleMedium;
+      break;
+    default:
+      return null;
+  }
+
+  return css`
+    height: ${height};
+    border-radius: ${borderRadius};
+    font-style: ${fontStyle};
+  `;
+};
