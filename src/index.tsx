92cc96ebf7035963381576c9453c6418861dc7f5
mod_siw <pvq_71@ewhain.net>
2024-01-18 00:22:21 +0900

    [feat] recoil 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './style/theme';
-import GlobalStyle from './style/GlobalStyle';
-import './assets/fonts/font.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
+import GlobalStyle from "./style/GlobalStyle";
+import { RecoilRoot } from "recoil";
+import "./assets/fonts/font.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </RecoilRoot>
 );
